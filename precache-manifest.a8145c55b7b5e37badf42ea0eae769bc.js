efd94019d305ec053d73c662abf9522e31f24623
AnnZeller <annazeller@mail.ru>
2020-10-04 21:01:08 +0300

    Updates

---
+++ precache-manifest.a8145c55b7b5e37badf42ea0eae769bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f052caa3eccb0879325f9eb0b07bd91b",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "91a65ef4df086f43a176",
+    "url": "/test-app/static/css/main.d43e42eb.chunk.css"
+  },
+  {
+    "revision": "1f6030ae21d44762be50",
+    "url": "/test-app/static/js/2.631197d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-app/static/js/2.631197d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a65ef4df086f43a176",
+    "url": "/test-app/static/js/main.6afe2780.chunk.js"
+  },
+  {
+    "revision": "010976abd684515c15d4",
+    "url": "/test-app/static/js/runtime-main.107bf183.js"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/test-app/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/test-app/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "2786ee486e9d789a9f2ff04d3b4cd5fd",
+    "url": "/test-app/static/media/Vector (Stroke).2786ee48.svg"
+  },
+  {
+    "revision": "a2f51fff44bead6554e15c5f38b6df05",
+    "url": "/test-app/static/media/play.a2f51fff.svg"
+  },
+  {
+    "revision": "079d28e6fee93e021cda4e8dbe2f1892",
+    "url": "/test-app/static/media/plus.079d28e6.svg"
+  },
+  {
+    "revision": "138d4892c059b2750b5959132f9d65cd",
+    "url": "/test-app/static/media/x.138d4892.svg"
+  }
+]);
